de6cc0b46730e2ff4100e4f167071373a6430d60
Richard Wang <richard2000wang@outlook.com>
2021-04-04 20:34:12 -0700

    Created Java script file and added basic notes

---
+++ app.js
@@ -0,0 +1,10 @@
+/* function to add save notes in an array */
+    /*create array*/
+    /*take in user input, google how to exactly do this*/
+    /*place user input into the array*/
+    /*after user is done, create another array*/
+
+/* function to add note titles to the side menu */
+
+
+/* function to display the note when the note title is clicked */
