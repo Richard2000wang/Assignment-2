15bea2f56a7fabac8a891a3cc35a0474177bb4a4
Richard Wang <richard2000wang@outlook.com>
2021-04-04 21:02:44 -0700

    Add details to java script notes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 /* function to add save notes in an array */
-    /*create array*/
-    /*take in user input, google how to exactly do this*/
+    /*on event click of button, create array*/
+    /*take in user input via events, google how to exactly do this*/
     /*place user input into the array*/
-    /*after user is done, create another array*/
-
-/* function to add note titles to the side menu */
-
+    /*remove textbox, save button and go back to the new note button*/
+    /*if user presses save again, go back to step one*/
 
 /* function to display the note when the note title is clicked */
+    /* On event click of button, use Element.insertAdjacentHTML to display note next to the textbox, or wherever appropriate */
+
+/* function of a button to change the color to dark mode on click */
+    /* use Element.insertAdjacentHTML to add classes with different colors for dark mode*/
+    /* replace button with another button that turns it back to light mode on event click */
+    /* Make sure to have appropriate CSS code so the buttons work*/
+    /* Find out if there is a better way than to replace all the classes*/
